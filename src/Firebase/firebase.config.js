c9802ae8c0f0e1107a8d2270af749ee414d5bc5d
arunabhnewar <newardevil@gmail.com>
2021-10-24 00:49:50 +0600

    img added

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyD7ES-wMCpqKoIe4SM8Ix-wmLy_pdFQhaE",
-    authDomain: "fir-react-authentication-b8ad5.firebaseapp.com",
-    projectId: "fir-react-authentication-b8ad5",
-    storageBucket: "fir-react-authentication-b8ad5.appspot.com",
-    messagingSenderId: "825582886060",
-    appId: "1:825582886060:web:3d71d330b40454eb2b9309"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 export default firebaseConfig;
